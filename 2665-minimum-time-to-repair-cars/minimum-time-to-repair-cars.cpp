e45665ea1a896638b41069fa558ccc953804af05
Ajitverma650 <153760715+Ajitverma650@users.noreply.github.com>
2025-03-19 19:39:47 +0530

    Time: 55 ms (41.29%) | Memory: 59.4 MB (21.23%) - LeetSync

---
+++ 2665-minimum-time-to-repair-cars/minimum-time-to-repair-cars.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+   bool check(long long mid , vector<int>& nums, int cars)
+   {
+
+         long long  count =0;
+         for( int num :nums )
+         {
+             count+=sqrt(mid/num);
+         }
+          
+        return count>=cars;
+   }
+
+    
+
+    long long repairCars(vector<int>& ranks, int cars) {
+        
+
+     long long maxe=*max_element(ranks.begin(),ranks.end());
+
+     long long  end=maxe*cars*cars;
+     long long start=1;
+      long long  ans=-1;
+     while(start<=end)
+     {
+
+        long long  mid=start+(end-start)/2;
+        if(check(mid , ranks , cars))
+        {
+              ans=mid ;
+              end =mid-1;
+        }
+        else
+        {
+             start=mid+1;
+        }
+     }
+
+     return ans ;
+    }
+};
