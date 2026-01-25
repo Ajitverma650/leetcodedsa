4fc8317ccfc606533bca558f9fb8e60ab9d3f270
AJIT VERMA <153760715+Ajitverma650@users.noreply.github.com>
2026-01-25 22:16:35 +0530

    Time: 0 ms (100.00%) | Memory: 193.5 MB (15.31%) - LeetSync

--- 4199-minimum-prefix-removal-to-make-array-strictly-increasing/minimum-prefix-removal-to-make-array-strictly-increasing.cpp
+++ 4199-minimum-prefix-removal-to-make-array-strictly-increasing/minimum-prefix-removal-to-make-array-strictly-increasing.cpp
@@ -4,14 +4,11 @@ public:
 
         int n =nums.size();
 
-        if(n==2) 
-        {
-             return nums[0]>=nums[1]?1:0;
-        }
+        
 
         for( int i=n-2;i>=0;i--)
         {
-              if(nums[i+1]==nums[i]||nums[i]>nums[i+1])
+              if(nums[i]>=nums[i+1])
               {
 
 
